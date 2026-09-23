c2f089f1558586a40bfb08492078ba67c5f0d1d8
Douaa Ouguzine <ouguzine.douaa@gmail.com>
2026-09-23 11:08:14 +0200

    <build> : Complete function

--- notebook.py
+++ notebook.py
@@ -34,8 +34,21 @@ def _(pytest):
     return
 
 
+@app.function
+def fibonacci_v2(n): 
+    if n == 0 : 
+        return 0
+    if n == 1 : 
+        return 1 
+    return fibonacci_v2(n-1) + fibonacci_v2(n-2)
+
+
 @app.cell
-def _():
+def _(pytest):
+    @pytest.mark.parametrize(("n", "expected"), [(2, 1), (3,2), (4, 3)],)
+    def test_cases2(n, expected):
+        assert fibonacci_v2(n) == expected
+
     return
 
 
